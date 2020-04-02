547f1894bdcff09312e6c7839558295ec8830da6
Sonal Kothari <51846416+sksonalkothari@users.noreply.github.com>
2020-04-02 14:57:12 +0530

    Removed unnecessary code

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,6 @@
-console.log('client side js file is loaded')
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-
 const message1 = document.querySelector('#message-1')
-//message1.textContent = 'From javascript'
-
 const message2 = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (event) => {
