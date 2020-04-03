f1a37110501ceab758f8f96177cff874ab165538
Sonal Kothari <51846416+sksonalkothari@users.noreply.github.com>
2020-04-03 14:17:32 +0530

    updated forecast string to include temperature low and high

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback(body.error, undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain')
+            callback(undefined, body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain. Today\'s highest temperature is '+body.daily.data[0].temperatureHigh+' and lowest temperature is '+body.daily.data[0].temperatureLow)
         }
     })
 }
